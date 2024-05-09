9eae46d21e2c6bd14bd657e587679a51f7ce1953
k023g <k023g@MSI>
2024-05-09 23:43:49 +0900

    プロジェクト ファイルを追加します。

---
+++ Result.cpp
@@ -0,0 +1,32 @@
+#include "Result.h"
+
+Result::Result()
+{
+}
+
+void Result::Initialize()
+{
+	cameraTranslate = { 0.0f,1.9f,-6.49f };
+	cameraRotate = { 0.26f,0.0f,0.0f };
+	rotate = {};
+	translate = {1.0f,10.0f,5.0f};
+	cameraPosition = { 0.0f,0.0f,-5.0f };
+	worldMatrix = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, rotate, translate);
+	cameraMatrix = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, { 0.0f,0.0f,0.0f }, cameraPosition);
+	viewMatrix = Inverse(cameraMatrix);
+	projectionMatrix = MakePerspectiveFovMatrix(0.45f, float(kWindowWidth) / float(kWindowHeight), 0.1f, 100.0f);
+	worldviewProjectionMatrix = Multiply(worldMatrix, Multiply(viewMatrix, projectionMatrix));
+	viewportMatrix = MakeViewportMatrix(0, 0, float(kWindowWidth), float(kWindowHeight), 0.0f, 1.0f);
+	
+}
+
+void Result::Updata()
+{
+	
+}
+
+void Result::Draw()
+{
+	DrawGrid(viewMatrix,viewportMatrix);
+	//DrawSphere();
+}
